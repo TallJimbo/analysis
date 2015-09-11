311a255d01835a823902100875de73ae8d53aac1
Jim Bosch <jbosch@astro.princeton.edu>
2015-09-12 05:46:13 +0900

    Bugfixes and minor improvements to catalog loader

--- analysis/catalogs.py
+++ analysis/catalogs.py
@@ -4,16 +4,16 @@ import lsst.afw.table
 
 class MagConverter(object):
 
-    def __init__(self, flux, schemaMapper, mag=None, prefix=None):
+    def __init__(self, flux, schema, mag=None, prefix=None):
         if mag is None:
             mag = flux.replace("flux", "mag")
         if prefix is not None:
             mag = "%s.%s" % (prefix, mag)
-        self.fluxKey = schemaMapper.getInputSchema().find(flux).key
-        self.fluxErrKey = schemaMapper.getInputSchema().find(flux + ".err").key
-        self.magKey = schemaMapper.editOutputSchema().addField(mag, type=float, doc="magnitude for %s" % flux)
-        self.magErrKey = schemaMapper.editOutputSchema().addField(mag + ".err", type=float,
-                                                                  doc="magnitude err for %s" % flux)
+            flux = "%s.%s" % (prefix, flux)
+        self.fluxKey = schema.find(flux).key
+        self.fluxErrKey = schema.find(flux + ".err").key
+        self.magKey = schema.addField(mag, type=float, doc="magnitude for %s" % flux)
+        self.magErrKey = schema.addField(mag + ".err", type=float, doc="magnitude err for %s" % flux)
 
     def __call__(self, catalog, calib):
         oldThrow = calib.getThrowOnNegativeFlux()
@@ -39,7 +39,7 @@ class CatalogLoader(object):
     def __init__(self, butler, filters=("g", "r", "i", "z", "y"), forced=True, meas=True):
         self.butler = butler
         self.filters = tuple(filters)
-        refSchema = butler.get("deepCoadd_ref_schema", immediate=True)
+        refSchema = butler.get("deepCoadd_ref_schema", immediate=True).schema
         self.refMapper = lsst.afw.table.SchemaMapper(refSchema)
         self.refMapper.addMinimalSchema(lsst.afw.table.SourceTable.makeMinimalSchema(), True)
         sharedFieldsAlreadyMapped = set(lsst.afw.table.SourceTable.makeMinimalSchema().getNames())
@@ -55,25 +55,26 @@ class CatalogLoader(object):
         self.measMappers = dict()
         self.measMags = dict()
         if meas:
-            measSchema = butler.get("deepCoadd_meas_schema", immediate=True)
+            measSchema = butler.get("deepCoadd_meas_schema", immediate=True).schema
             for b in self.filters:
-                        continue
                 self.measMappers[b] = lsst.afw.table.SchemaMapper(measSchema, self.outSchema)
                 for item in measSchema:
                     if item.field.getName() in sharedFieldsAlreadyMapped:
+                        continue
                     prefix = item.field.getName().split(".")[0]
                     if prefix in self.SHARED_PREFIXES:
                         self.measMappers[b].addMapping(item.key, "%s.%s" % (b, item.field.getName()))
-                        sharedFieldsAlreadyMapped.add(item.field.getName())
+                        sharedFieldsAlreadyMapped.add("%s.%s" % (b, item.field.getName()))
                     else:
                         self.measMappers[b].addMapping(item.key, "meas.%s.%s" % (b, item.field.getName()))
-                self.measMags[b] = [MagConverter(flux, self.measMappers[b], prefix="meas.%s" % b)
-                                    for flux in self.MAG_FIELDS]
-                self.outSchema = measMappers[b].getOutputSchema()
+                self.measMags[b] = [MagConverter(flux, self.measMappers[b].editOutputSchema(), prefix="meas.%s" % b)
+                                    for flux in self.MAG_FIELDS
+                                    if flux in measSchema.getNames()]
+                self.outSchema = self.measMappers[b].getOutputSchema()
         self.forcedMappers = dict()
         self.forcedMags = dict()
         if forced:
-            forcedSchema = butler.get("deepCoadd_forced_src_schema", immediate=True)
+            forcedSchema = butler.get("deepCoadd_forced_src_schema", immediate=True).schema
             for b in self.filters:
                 self.forcedMappers[b] = lsst.afw.table.SchemaMapper(forcedSchema, self.outSchema)
                 for item in forcedSchema:
@@ -84,13 +85,14 @@ class CatalogLoader(object):
                         self.forcedMappers[b].addMapping(item.key, "%s.%s" % (b, item.field.getName()))
                     else:
                         self.forcedMappers[b].addMapping(item.key, "forced.%s.%s" % (b, item.field.getName()))
-                self.forcedMags[b] = [MagConverter(flux, self.forcedMappers[b], prefix="forced.%s" % b)
-                                      for flux in self.MAG_FIELDS]
-                self.outSchema = forcedMappers[b].getOutputSchema()
+                self.forcedMags[b] = [MagConverter(flux, self.forcedMappers[b].editOutputSchema(), prefix="forced.%s" % b)
+                                      for flux in self.MAG_FIELDS
+                                      if flux in forcedSchema.getNames()]
+                self.outSchema = self.forcedMappers[b].getOutputSchema()
 
-    def read(self, *args, tracts=(), tract=None, patches=(), patch=None, filters=None, filter=None,
-             extend=None, copy=True):
-        dataIDs = list(args)
+    def read(self, dataIds=(), tracts=(), tract=None, patches=(), patch=None, filters=None, filter=None,
+             extend=None, copy=True, progress=False):
+        dataIDs = list(dataIds)
         if filters is None:
             if filter is None:
                 filters = self.filters
@@ -111,15 +113,27 @@ class CatalogLoader(object):
             catalog.extend(extend)
         else:
             catalog = lsst.afw.table.SourceCatalog(self.outSchema)
-        for dataID in dataIDs:
+        if progress:
+            try:
+                from IPython.html.widgets import FloatProgress
+                from IPython.display import display
+                progressBar = FloatProgress(min=0, max=len(dataIDs))
+                display(progressBar)
+            except RuntimeError:
+                progressBar = None
+        for n, dataID in enumerate(dataIDs):
             subCat = lsst.afw.table.SourceCatalog(catalog.table)
             refCat = self.butler.get("deepCoadd_ref", immediate=True,
                                      flags=lsst.afw.table.SOURCE_IO_NO_FOOTPRINTS, **dataID)
-            subCat.extend(refCat, self.refMapper)
+            subCat.extend(refCat, mapper=self.refMapper)
             subCat[self.tractKey][:] = dataID["tract"]
-            patchX, patchY = int(p) for p in dataID["patch"].split(",")
+            patchX, patchY = (int(p) for p in dataID["patch"].split(","))
             subCat[self.patchXKey][:] = patchX
             subCat[self.patchXKey][:] = patchY
+            calibs = dict()
+            for b in self.filters:
+                coadd = self.butler.get("deepCoadd", immediate=True, filter="HSC-%s" % b.upper(), **dataID)
+                calibs[b] = coadd.getCalib()
             for b, mapper in self.measMappers.iteritems():
                 measCat = self.butler.get("deepCoadd_meas", immediate=True,
                                           flags=lsst.afw.table.SOURCE_IO_NO_FOOTPRINTS,
@@ -132,7 +146,19 @@ class CatalogLoader(object):
                                           filter="HSC-%s" % b.upper(), **dataID)
                 for inRecord, outRecord in zip(forcedCat, subCat):
                     outRecord.assign(inRecord, mapper)
-            catalog.extend(subCat)
+            for b, magc in self.measMags.iteritems():
+                for m in magc:
+                    m(subCat, calibs[b])
+            for b, magc in self.forcedMags.iteritems():
+                for m in magc:
+                    m(subCat, calibs[b])
+            catalog.extend(subCat, False)
+            if progress:
+                if progressBar is not None:
+                    progressBar.value = n + 1
+                else:
+                    print "Loaded %s (%d of %d)" % (dataID, n + 1, len(dataIDs))
+        catalog.sort()
         if copy:
             catalog = catalog.copy(deep=True)
         return catalog
